c9ac4e765f1307f73aade149a5f8665951a0804e
Riccardo <riccardo_london@live.it>
2019-03-17 09:04:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ require_relative './author.rb'
 class Song
   attr_accessor :title, :artist
 
-  def initialize(title=nil, artist)
+  def initialize(title=nil, artist = nil)
     @title = title
     @artist = artist
   end
