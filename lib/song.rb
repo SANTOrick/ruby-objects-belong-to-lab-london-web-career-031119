06340f98592b15b3ceaf8c2945e4417617204a20
Riccardo <riccardo_london@live.it>
2019-03-17 08:49:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+require_relative './artist.rb'
+require_relative './post.rb'
+require_relative './author.rb'
+
+class Song
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    
+  end
+
+
+
+end
