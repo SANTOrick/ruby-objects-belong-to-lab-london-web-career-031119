c7b2ef879fcf0fa749efc28dcaea5e003d06b875
Riccardo <riccardo_london@live.it>
2019-03-17 09:05:33 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,18 @@
 require_relative './artist.rb'
 require_relative './author.rb'
 require_relative './song.rb'
+
+class Post
+attr_accessor :author
+
+  def initialize (author = nil)
+    @author = author
+  end
+
+  def self.author=
+    self.author = Author.new
+
+
+
+
+end
